ffdaab5938378384e149c18fa060f4513b5ae5cc
Joao Vitor <jvmelop@gmail.com>
2025-07-15 12:59:45 -0300

    fix: update image URLs and professional names in constants

--- packages/core/src/constants/professionals.ts
+++ packages/core/src/constants/professionals.ts
@@ -15,7 +15,7 @@ const professionals: Professional[] = [
     name: 'Brutal Beto',
     description:
       "When it comes to beards, no one does it better than Brutal Beto! Its sharp blades are legendary and its style is unmistakable. For a striking and robust look, he's your man!",
-    imageURL: '/professionals/professional-2.jpg',
+    imageURL: '/professionals/professional-3.jpg',
     rating: 4.5,
     totalReviews: 39,
   },
@@ -24,7 +24,7 @@ const professionals: Professional[] = [
     name: 'Scissored Kathya',
     description:
       'Kathya is not afraid to scissor! With precision and boldness, she transforms any hair into a rustic masterpiece. Her motto? Cut roots without losing femininity!',
-    imageURL: '/professionals/professional-3.jpg',
+    imageURL: '/professionals/professional-4.jpg',
     rating: 5,
     totalReviews: 91,
   },
@@ -33,16 +33,16 @@ const professionals: Professional[] = [
     name: 'Sawyer Sérgio',
     description:
       'Sérgio is known for his impeccable technique and unique style. With his saw (or rather, scissors) in hand, he creates visuals that impress. A well-groomed beard is up to him!',
-    imageURL: '/professionals/professional-4.jpg',
+    imageURL: '/professionals/professional-5.jpg',
     rating: 4.5,
     totalReviews: 29,
   },
   {
     id: 5,
-    name: 'Rafa Shave Everything',
+    name: 'Rafa Shave It All',
     description:
       'For those who prefer a closer look, Rafa is the expert! His machine shaves everything with precision, leaving a perfect finish. Shaving is his art!',
-    imageURL: '/professionals/professional-5.jpg',
+    imageURL: '/professionals/professional-6.jpg',
     rating: 4.5,
     totalReviews: 44,
   },
@@ -51,7 +51,7 @@ const professionals: Professional[] = [
     name: 'Fernanda Spark',
     description:
       "Fernanda brings a touch of shine and energy to every cut. With her agile blades, she works miracles on any hair type. Need a look that's full of sparkle? She's the one for you!",
-    imageURL: '/professionals/professional-6.jpg',
+    imageURL: '/professionals/professional-7.jpg',
     rating: 5,
     totalReviews: 46,
   },
@@ -60,7 +60,7 @@ const professionals: Professional[] = [
     name: 'Ax Marco',
     description:
       "Hail, hail, guys! I'm Ax Marco, the only hairdresser who can transform a lion's mane into a cut fit for a king with a single movement of scissors.",
-    imageURL: '/professionals/professional-7.jpg',
+    imageURL: '/professionals/professional-2.jpg',
     rating: 5,
     totalReviews: 87,
   },
